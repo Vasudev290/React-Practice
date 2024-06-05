495f089089fb4955329621e660e8bd701e33be35
vasudev290 <vasukesavulu@gmail.com>
2024-06-05 16:16:06 +0530

    tst

---
+++ React Practice/React-3/src/index.js
@@ -0,0 +1,8 @@
+import ReactDOM from 'react-dom'
+import App from './app'
+import'../node_modules/bootstrap/dist/css/bootstrap.css'
+
+ReactDOM.render(<App />, document.getElementById("root"))
+
+
+   
